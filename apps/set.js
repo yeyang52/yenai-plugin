55f31650b2240725a1c297b2c13fdb6ecddc1aae
yeyang <746659424@qq.com>
2022-10-06 14:57:05 +0800

    修改设置删除缓存时间

--- apps/set.js
+++ apps/set.js
@@ -64,7 +64,7 @@ export class NewConfig extends plugin {
 
         if (time < 120) return e.reply('❎ 时间不能小于两分钟')
 
-        let res = await redis.set(`yenai:notice:deltime`, Number(time))
+        let res = await redis.set(`yenai:notice:deltime`, String(time[0]))
         if (res == "OK") {
             e.reply(`✅ 已设置删除缓存时间为${getsecond(time)}`)
         }
@@ -118,4 +118,36 @@ const configs = {
     禁言: "botBeenBanned",
     全部通知: "notificationsAll",
     删除缓存: "deltime"
+}
+// 秒转换
+function getsecond(value) {
+    let secondTime = parseInt(value) // 秒
+    let minuteTime = 0 // 分
+    let hourTime = 0 // 小时
+    if (secondTime > 60) {
+        // 如果秒数大于60，将秒数转换成整数
+        // 获取分钟，除以60取整数，得到整数分钟
+        minuteTime = parseInt(secondTime / 60)
+        // 获取秒数，秒数取佘，得到整数秒数
+        secondTime = parseInt(secondTime % 60)
+        // 如果分钟大于60，将分钟转换成小时
+        if (minuteTime > 60) {
+            // 获取小时，获取分钟除以60，得到整数小时
+            hourTime = parseInt(minuteTime / 60)
+            // 获取小时后取佘的分，获取分钟除以60取佘的分
+            minuteTime = parseInt(minuteTime % 60)
+        }
+    }
+    // 处理返回消息
+    let result = ''
+    if (secondTime != 0) {
+        result = parseInt(secondTime) + '秒'
+    }
+    if (minuteTime > 0) {
+        result = parseInt(minuteTime) + '分' + result
+    }
+    if (hourTime > 0) {
+        result = parseInt(hourTime) + '小时' + result
+    }
+    return result
 }

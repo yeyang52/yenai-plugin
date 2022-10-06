296edad1f6e54357efead1fd9555a38a77256c51
yeyang <746659424@qq.com>
2022-10-07 03:22:17 +0800

    修改sendlink

--- apps/sendLike.js
+++ apps/sendLike.js
@@ -20,7 +20,7 @@ export class example extends plugin {
       /** https://oicqjs.github.io/oicq/#events */
       event: 'message',
       /** 优先级，数字越小等级越高 */
-      priority: 5000,
+      priority: 2000,
       rule: [
         {
           /** 命令正则匹配 */

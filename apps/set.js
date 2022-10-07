676ef20d38a1019500ad3e1e348ebb3c53fefd4d
yeyang <746659424@qq.com>
2022-10-07 16:12:29 +0800

    修改帮助

--- apps/set.js
+++ apps/set.js
@@ -1,9 +1,6 @@
 import plugin from '../../../lib/plugins/plugin.js'
 import fs from "fs";
 import lodash from "lodash";
-import { createRequire } from "module";
-import { exec } from "child_process";
-import { Cfg } from "../components/index.js";
 import Common from "../components/Common.js";
 
 
@@ -137,6 +134,7 @@ const rodom = async function () {
     var imgs = list_img.length == 1 ? list_img[0] : list_img[lodash.random(0, list_img.length - 1)];
     return imgs;
 }
+
 const getStatus = function (rote) {
     if (rote) {
         return `<div class="cfg-status" >已开启</div>`;
@@ -145,6 +143,7 @@ const getStatus = function (rote) {
     }
 
 }
+
 const configs = {
     好友消息: "privateMessage",
     群消息: "groupMessage",
@@ -165,36 +164,4 @@ const configs = {
     禁言: "botBeenBanned",
     全部通知: "notificationsAll",
     删除缓存: "deltime"
-}
-// 秒转换
-function getsecond(value) {
-    let secondTime = parseInt(value) // 秒
-    let minuteTime = 0 // 分
-    let hourTime = 0 // 小时
-    if (secondTime > 60) {
-        // 如果秒数大于60，将秒数转换成整数
-        // 获取分钟，除以60取整数，得到整数分钟
-        minuteTime = parseInt(secondTime / 60)
-        // 获取秒数，秒数取佘，得到整数秒数
-        secondTime = parseInt(secondTime % 60)
-        // 如果分钟大于60，将分钟转换成小时
-        if (minuteTime > 60) {
-            // 获取小时，获取分钟除以60，得到整数小时
-            hourTime = parseInt(minuteTime / 60)
-            // 获取小时后取佘的分，获取分钟除以60取佘的分
-            minuteTime = parseInt(minuteTime % 60)
-        }
-    }
-    // 处理返回消息
-    let result = ''
-    if (secondTime != 0) {
-        result = parseInt(secondTime) + '秒'
-    }
-    if (minuteTime > 0) {
-        result = parseInt(minuteTime) + '分' + result
-    }
-    if (hourTime > 0) {
-        result = parseInt(hourTime) + '小时' + result
-    }
-    return result
 }
--- model/Config.js
+++ model/Config.js
@@ -46,6 +46,38 @@ class Config {
             await common.sleep(200)
         }
     }
+    // 秒转换
+    getsecond(value) {
+        let secondTime = parseInt(value) // 秒
+        let minuteTime = 0 // 分
+        let hourTime = 0 // 小时
+        if (secondTime > 60) {
+            // 如果秒数大于60，将秒数转换成整数
+            // 获取分钟，除以60取整数，得到整数分钟
+            minuteTime = parseInt(secondTime / 60)
+            // 获取秒数，秒数取佘，得到整数秒数
+            secondTime = parseInt(secondTime % 60)
+            // 如果分钟大于60，将分钟转换成小时
+            if (minuteTime > 60) {
+                // 获取小时，获取分钟除以60，得到整数小时
+                hourTime = parseInt(minuteTime / 60)
+                // 获取小时后取佘的分，获取分钟除以60取佘的分
+                minuteTime = parseInt(minuteTime % 60)
+            }
+        }
+        // 处理返回消息
+        let result = ''
+        if (secondTime != 0) {
+            result = parseInt(secondTime) + '秒'
+        }
+        if (minuteTime > 0) {
+            result = parseInt(minuteTime) + '分' + result
+        }
+        if (hourTime > 0) {
+            result = parseInt(hourTime) + '小时' + result
+        }
+        return result
+    }
 }
 
 

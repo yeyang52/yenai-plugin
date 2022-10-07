769ffb2f0326a0fc540b08b2dbd130f5f1f0c308
yeyang <746659424@qq.com>
2022-10-07 16:31:48 +0800

    修改帮助

--- config/system/help_system.js
+++ config/system/help_system.js
@@ -26,103 +26,116 @@ export const helpCfg = {
 export const helpList = [{
   "group": "助手",
   "list": [{
-    "icon": 61,
+    "icon": 1,
     "title": "#发好友 <QQ> <消息>", "desc": "给好友发送一条涩涩的消息"
   },
   {
-    "icon": 64,
+    "icon": 2,
     "title": "#发群聊 <群号> <消息>",
     "desc": "给群聊发送一条涩涩的消息"
   },
   {
-    "icon": 66,
+    "icon": 3,
     "title": "#改头像 <图片>",
     "desc": "顾名思义"
   },
   {
-    "icon": 62, "title": "#改状态 <状态> ",
+    "icon": 4,
+    "title": "#改状态 <状态> ",
     "desc": "顾名思义"
   },
   {
-    "icon": 58, "title": "#改昵称 <昵称> ",
+    "icon": 5, "title": "#改昵称 <昵称> ",
     "desc": "顾名思义"
   },
   {
-    "icon": 59, "title": "#改签名 <签名> ",
+    "icon": 6, "title": "#改签名 <签名> ",
     "desc": "顾名思义"
   },
   {
     "title": "#改性别 <性别> ",
-    "desc": "顾名思义", "icon": 71
+    "desc": "顾名思义",
+    "icon": 7
   },
   {
     "title": "#改群名片 <名片> ",
-    "desc": "群里Bot自己的名片", "icon": 29
+    "desc": "群里Bot自己的名片",
+    "icon": 8
   },
   {
     "title": "#改群昵称 <昵称>",
-    "desc": "改群的昵称", "icon": 57
+    "desc": "改群的昵称", "icon": 9
   },
   {
     "title": "#改群头像 <图片>",
-    "desc": "顾名思义", "icon": 46
+    "desc": "顾名思义",
+    "icon": 10
   },
   {
     "title": "#删好友 <QQ> ",
-    "desc": "删掉涩涩的好友", "icon": 71
+    "desc": "删掉涩涩的好友",
+    "icon": 11
   },
   {
     "title": "#退群 <群号> ",
-    "desc": "退掉涩涩的群", "icon": 32
+    "desc": "退掉涩涩的群",
+    "icon": 12
   },
   {
     "title": "#获取群列表",
-    "desc": "获取Bot的所有群", "icon": 30
+    "desc": "获取Bot的所有群",
+    "icon": 13
   },
   {
     "title": "#获取好友列表",
-    "desc": "获取Bot的所有好友", "icon": 54
+    "desc": "获取Bot的所有好友",
+    "icon": 14
   },
   {
     "title": "#取说说列表 <页数> ",
-    "desc": "获取Bot的说说列表", "icon": 29
+    "desc": "获取Bot的说说列表",
+    "icon": 15
   },
   {
     "title": "#发说说 <内容> ",
-    "desc": "发送一条涩涩的说说", "icon": 71
+    "desc": "发送一条涩涩的说说",
+    "icon": 16
   },
   {
     "title": "#删说说 <序号>",
-    "desc": "用取说说列表获取序号", "icon": 54
+    "desc": "用取说说列表获取序号",
+    "icon": 17
   },
   {
     "title": "#清空说说",
-    "desc": "一键清空", "icon": 48
+    "desc": "一键清空",
+    "icon": 18
   },
   {
     "title": "#清空留言",
-    "desc": "一键清空留言", "icon": 57
+    "desc": "一键清空留言",
+    "icon": 19
   }]
 },
 {
   "group": "娱乐功能",
   "list": [{
-    "icon": 32,
+    "icon": 20,
     "title": "#唱歌",
     "desc": "随机唱鸭"
   },
   {
-    "icon": 12,
+    "icon": 5,
     "title": "#赞我",
     "desc": "给你点一个大大的赞"
   },
   {
-    "icon": 20,
+    "icon": 7,
     "title": "#椰羊收益曲线",
     "desc": "查看角色收益曲线"
   },
   {
-    "icon": 25,
+    "icon": 9,
     "title": "#收益曲线帮助",
     "desc": "=-="
   }]
@@ -131,22 +144,22 @@ export const helpList = [{
   "group": "管理命令，仅管理员可用",
   "auth": "master",
   "list": [{
-    "icon": 32,
+    "icon": 8,
     "title": "#椰奶设置",
     "desc": "查看椰奶设置"
   },
   {
-    "icon": 42,
+    "icon": 1,
     "title": "#椰奶(强制)更新",
     "desc": "更新椰奶"
   },
   {
-    "icon": 35,
+    "icon": 15,
     "title": "#椰奶版本",
     "desc": "查看版本信息"
   },
   {
-    "icon": 35,
+    "icon": 12,
     "title": "#椰奶更新日志",
     "desc": "查看更新日志"
   },]

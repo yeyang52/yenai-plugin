4a59f440a44447630ae5731a8eafac4bf925c674
yeyang <746659424@qq.com>
2022-10-12 02:57:05 +0800

    运行状态初次提交

---
+++ apps/state.js
@@ -0,0 +1,182 @@
+import plugin from '../../../lib/plugins/plugin.js'
+import moment from 'moment';
+import os from 'os';
+import { Version, Common, Plugin_Name } from '../components/index.js'
+import CPU from '../model/cpu.js';
+// import process from 'process';
+export class example extends plugin {
+  constructor() {
+    super({
+      /** 功能名称 */
+      name: '状态',
+      /** 功能描述 */
+      dsc: '状态',
+      /** https://oicqjs.github.io/oicq/#events */
+      event: 'message',
+      /** 优先级，数字越小等级越高 */
+      priority: 5000,
+      rule: [
+        {
+          /** 命令正则匹配 */
+          reg: '^运行状态$',
+          /** 执行方法 */
+          fnc: 'state'
+        }
+      ]
+
+    })
+  }
+
+
+  async state(e) {
+    let portrait = `https://q1.qlogo.cn/g?b=qq&s=0&nk=${Bot.uin}`
+    //cpu使用率
+    let cpu_info = await CPU.getCPUUsage()
+    //内存使用率
+    let MemUsage = (1 - os.freemem() / os.totalmem()).toFixed(2)
+    //空闲内存
+    let freemem = CPU.getfilesize(os.freemem())
+
+    //使用内存
+    let Usingmemory = CPU.getfilesize((os.totalmem() - os.freemem()))
+
+    //cpu
+    let cpu = Circle(cpu_info)
+    let [cpu_leftCircle, cpu_rightCircle] = cpu
+    //ram
+    let ram = Circle(MemUsage)
+    let [ram_leftCircle, ram_rightCircle] = ram
+    //最大mhz
+    let maxspeed = CPU.getmaxspeed()
+    //核心
+    let hx = os.cpus().length + "核"
+    let data = {
+      //路径
+      tplFile: `./plugins/yenai-plugin/resources/state/state.html`,
+      //头像
+      portrait,
+      //运行时间
+      runTime: await this.statusTime(),
+      //版本
+      version: Version.ver,
+      //地址
+      dz: process.cwd(),
+      //昵称
+      nickname: Bot.nickname,
+      //系统运行时间
+      systime: Formatting(),
+      //收
+      recv: Bot.statistics.recv_msg_cnt,
+      // 发
+      sent: Bot.statistics.sent_msg_cnt,
+      //cpu占比
+      cpu_leftCircle,
+      cpu_rightCircle,
+      cpu_info: cpu_info * 100 + "%",
+      //核心
+      hx,
+      //最大MHZ
+      maxspeed,
+      //系统名
+      hostname: os.type(),
+      //内存使用率
+      ram_leftCircle,
+      ram_rightCircle,
+      MemUsage: MemUsage * 100 + "%",
+      //空闲内存
+      freemem,
+      //已用内存
+      Usingmemory,
+      //nodejs运行时间
+      uptime: getuptime(),
+      //nodejs版本
+      nodeversion: process.version,
+      //网络
+      network: Object.keys(os.networkInterfaces())[0]
+    }
+
+    await Common.render('state/state', {
+      ...data,
+    }, {
+      e,
+      scale: 2.0
+    })
+  }
+
+  async statusTime() {
+    let runTime = moment().diff(moment.unix(Bot.stat.start_time), 'seconds')
+    let Day = Math.floor(runTime / 3600 / 24)
+    let Hour = Math.floor((runTime / 3600) % 24)
+    let Min = Math.floor((runTime / 60) % 60)
+    if (Day > 0) {
+      runTime = `${Day}:${Hour}:${Min}`
+    } else {
+      runTime = `${Hour}:${Min}`
+    }
+    return runTime
+  }
+}
+
+function Formatting() {
+  let second = os.uptime()
+  //  分
+  let minute = 0
+  //  小时
+  let hour = 0
+  //  天
+  let day = 0
+  //  如果秒数大于60，将秒数转换成整数
+  if (second > 60) {
+    //  获取分钟，除以60取整数，得到整数分钟
+    minute = parseInt(second / 60)
+    //  获取秒数，秒数取佘，得到整数秒数
+    second = parseInt(second % 60)
+    //  如果分钟大于60，将分钟转换成小时
+    if (minute > 60) {
+      //  获取小时，获取分钟除以60，得到整数小时
+      hour = parseInt(minute / 60)
+      //  获取小时后取佘的分，获取分钟除以60取佘的分
+      minute = parseInt(minute % 60)
+      //  如果小时大于24，将小时转换成天
+      if (hour > 23) {
+        //  获取天数，获取小时除以24，得到整天数
+        day = parseInt(hour / 24)
+        //  获取天数后取余的小时，获取小时除以24取余的小时
+        hour = parseInt(hour % 24)
+      }
+    }
+    hour = hour < 10 ? "0" + hour : hour
+    minute = minute < 10 ? "0" + minute : minute
+    second = second < 10 ? "0" + second : second
+    return day + "天 " + hour + ":" + minute + ":" + second
+  }
+}
+
+function Circle(res) {
+  let num = (res * 360).toFixed(0)
+  let leftCircle = `style=transform:rotate(-180deg)`;
+  let rightCircle;
+  if (num > 180) {
+    leftCircle = `style=transform:rotate(${num}deg)`
+  } else {
+    rightCircle = `style=transform:rotate(-${180 - num}deg)`;
+  }
+  return [leftCircle, rightCircle]
+}
+function getuptime() {
+  let second = process.uptime()
+  let minute = 0
+  let hour = 0
+  if (second > 60) {
+    minute = parseInt(second / 60)
+    second = parseInt(second % 60)
+    if (minute > 60) {
+      hour = parseInt(minute / 60)
+      minute = parseInt(minute % 60)
+    }
+    hour = hour < 10 ? "0" + hour : hour
+    minute = minute < 10 ? "0" + minute : minute
+    second = second < 10 ? "0" + second : second
+    return hour + ":" + minute + ":" + second
+  }
+}
---
+++ model/cpu.js
@@ -0,0 +1,92 @@
+///CPU利用率
+import os from 'os';
+//系统架构
+// console.log(os.arch());
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+class OSUtils {
+  constructor() {
+    this.cpuUsageMSDefault = 1000; // CPU 利用率默认时间段
+  }
+
+  /**
+  * 获取某时间段 CPU 利用率
+  * @param { Number } Options.ms [时间段，默认是 1000ms，即 1 秒钟]
+  * @param { Boolean } Options.percentage [true（以百分比结果返回）|false] 
+  * @returns { Promise }
+  */
+  async getCPUUsage(options) {
+    const that = this;
+    let cpuUsageMS = options;
+    cpuUsageMS = cpuUsageMS || that.cpuUsageMSDefault;
+    const t1 = that._getCPUInfo(); // t1 时间点 CPU 信息
+
+    await sleep(cpuUsageMS);
+
+    const t2 = that._getCPUInfo(); // t2 时间点 CPU 信息
+    const idle = t2.idle - t1.idle;
+    const total = t2.total - t1.total;
+    let usage = 1 - idle / total;
+
+    return usage.toFixed(2)
+  }
+
+  //    * 获取 CPU 瞬时时间信息
+  //    * @returns { Object } CPU 信息
+  //    * user <number> CPU 在用户模式下花费的毫秒数。
+  //    * nice <number> CPU 在良好模式下花费的毫秒数。
+  //    * sys <number> CPU 在系统模式下花费的毫秒数。
+  //    * idle <number> CPU 在空闲模式下花费的毫秒数。
+  //    * irq <number> CPU 在中断请求模式下花费的毫秒数。
+
+  _getCPUInfo() {
+    const cpus = os.cpus();
+    let user = 0, nice = 0, sys = 0, idle = 0, irq = 0, total = 0;
+
+    for (let cpu in cpus) {
+      const times = cpus[cpu].times;
+      user += times.user;
+      nice += times.nice;
+      sys += times.sys;
+      idle += times.idle;
+      irq += times.irq;
+    }
+
+    total += user + nice + sys + idle + irq;
+
+    return {
+      user,
+      sys,
+      idle,
+      total,
+    }
+  }
+
+  getmaxspeed() {
+
+    let res = os.cpus()
+    let max = res[0].speed
+    for (let i of res) {
+      if (i.speed > max) {
+        i.speed > max
+      }
+    }
+    return `最大${max}MHz`
+  }
+  getfilesize(size) {//把字节转换成正常文件大小
+    if (!size) return "";
+    var num = 1024.00; //byte
+    if (size < num)
+      return size + "B";
+    if (size < Math.pow(num, 2))
+      return (size / num).toFixed(2) + "KB"; //kb
+    if (size < Math.pow(num, 3))
+      return (size / Math.pow(num, 2)).toFixed(2) + "MB"; //M
+    if (size < Math.pow(num, 4))
+      return (size / Math.pow(num, 3)).toFixed(2) + "G"; //G
+    return (size / Math.pow(num, 4)).toFixed(2) + "T"; //T
+  }
+}
+// const cpuUsage = new OSUtils().getCPUUsage({ percentage: true });
+// console.log('cpuUsage: ', cpuUsage.then( data => console.log(data)), "\n");
+export default new OSUtils();

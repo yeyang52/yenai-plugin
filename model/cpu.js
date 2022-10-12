8d552e6943627eb0026c6a967d8927de33c26a91
yeyang <746659424@qq.com>
2022-10-12 16:03:29 +0800

    修改内存显示单位

--- model/cpu.js
+++ model/cpu.js
@@ -82,9 +82,9 @@ class OSUtils {
     if (size < num)
       return size + "B";
     if (size < Math.pow(num, 2))
-      return (size / num).toFixed(2) + "Kb"; //kb
+      return (size / num).toFixed(2) + "K"; //kb
     if (size < Math.pow(num, 3))
-      return (size / Math.pow(num, 2)).toFixed(2) + "Mb"; //M
+      return (size / Math.pow(num, 2)).toFixed(2) + "M"; //M
     if (size < Math.pow(num, 4))
       return (size / Math.pow(num, 3)).toFixed(2) + "G"; //G
     return (size / Math.pow(num, 4)).toFixed(2) + "T"; //T

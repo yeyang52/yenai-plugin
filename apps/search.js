88deb87c1d896c0af01a63026b71240015cc8493
yeyang <746659424@qq.com>
2022-11-29 11:57:05 +0800

    modified:   apps/search.js

--- apps/search.js
+++ apps/search.js
@@ -90,7 +90,7 @@ export class example extends plugin {
   }
   async search(e) {
     let regRet = searchReg.exec(e.msg)
-    if (/ip/ig.test(regRet[2])) return e.reply("(;｀O´)o警告！！触发屏蔽词！！！", true)
+    if (/(lp|ip)|((i|p|l)(地址|查询))/ig.test(regRet[2])) return e.reply("(;｀O´)o警告！！触发屏蔽词！！！", true)
     let url = SEARCH_MAP[regRet[1]] + encodeURIComponent(regRet[2])
     let res = await Browser.Webpage(url)
     logger.mark("开始搜索")

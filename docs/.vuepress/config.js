e003e4d05b332af10b3ea346c59b869a61cc34e8
yeyang <107110851+yeyang52@users.noreply.github.com>
2023-02-22 00:18:56 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
   themeConfig: {
     algolia: {
       apiKey: 'e06a8158e380032996667007566df42f',
-      indexName: 'yenai'
+      indexName: 'yenai',
       // 如果 Algolia 没有为你提供 `appId` ，使用 `BH4D9OD16A` 或者移除该配置项
       appId: '7AT6JPKZZA',
     },

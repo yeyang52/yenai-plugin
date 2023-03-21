f10971f69cd5c8b913a74d0f2bd0f686fd133b66
yeyang <746659424@qq.com>
2023-03-21 19:00:38 +0800

    🔧 更新锅巴配置

--- guoba.support.js
+++ guoba.support.js
@@ -32,36 +32,36 @@ export function supportGuoba () {
       // 配置项 schemas
       schemas: [
         {
-          component: 'Divider',
+          component: 'whole.Divider',
           label: '消息通知'
         },
         {
-          field: 'privateMessage',
+          field: 'whole.privateMessage',
           label: '好友消息',
           bottomHelpMessage: '开启后将转发好友消息，可进行回复',
           component: 'Switch'
         },
         {
-          field: 'groupMessage',
+          field: 'whole.groupMessage',
           label: '群聊消息',
           helpMessage: '开启后将转发全部群聊消息，建议配置单独群开启',
           bottomHelpMessage: '是否开启群聊消息通知',
           component: 'Switch'
         },
         {
-          field: 'grouptemporaryMessage',
+          field: 'whole.grouptemporaryMessage',
           label: '群临时消息',
           bottomHelpMessage: '开启后将转发群临时消息',
           component: 'Switch'
         },
         {
-          field: 'groupRecall',
+          field: 'whole.groupRecall',
           label: '群聊撤回',
           bottomHelpMessage: '群聊撤回后将撤回的消息转发给主人',
           component: 'Switch'
         },
         {
-          field: 'PrivateRecall',
+          field: 'whole.PrivateRecall',
           label: '好友撤回',
           bottomHelpMessage: '好友私聊撤回后将撤回的消息转发给主人',
           component: 'Switch'
@@ -71,21 +71,21 @@ export function supportGuoba () {
           label: '申请通知'
         },
         {
-          field: 'friendRequest',
+          field: 'whole.friendRequest',
           label: '好友申请',
           helpMessage: '将云崽的自动同意好友申请关闭后，可回复同意或拒绝进行处理',
           bottomHelpMessage: '是否开启好友申请通知',
           component: 'Switch'
         },
         {
-          field: 'addGroupApplication',
+          field: 'whole.addGroupApplication',
           label: '加群申请',
           helpMessage: '可回复同意或拒绝进行处理',
           bottomHelpMessage: '是否开启加群申请通知',
           component: 'Switch'
         },
         {
-          field: 'groupInviteRequest',
+          field: 'whole.groupInviteRequest',
           label: '群聊邀请',
           helpMessage: '将云崽的自动退群设置为0后，可回复同意或拒绝进行处理',
           bottomHelpMessage: '是否开启群聊邀请通知',
@@ -96,25 +96,25 @@ export function supportGuoba () {
           label: '列表变动'
         },
         {
-          field: 'groupAdminChange',
+          field: 'whole.groupAdminChange',
           label: '群管理变动',
           bottomHelpMessage: 'Bot被设置或取消管理，群员被设置或取消管理通知',
           component: 'Switch'
         },
         {
-          field: 'friendNumberChange',
+          field: 'whole.friendNumberChange',
           label: '好友列表变动',
           bottomHelpMessage: '新增好友和好友减少通知',
           component: 'Switch'
         },
         {
-          field: 'groupNumberChange',
+          field: 'whole.groupNumberChange',
           label: '群聊列表变动',
           bottomHelpMessage: '群转让，新增群聊，Bot退群，Bot被踢，群员被踢等通知',
           component: 'Switch'
         },
         {
-          field: 'groupMemberNumberChange',
+          field: 'whole.groupMemberNumberChange',
           label: '群成员变动',
           bottomHelpMessage: '新增群员，群员被踢，群员退群等通知',
           component: 'Switch'
@@ -124,14 +124,14 @@ export function supportGuoba () {
           label: '其他通知'
         },
         {
-          field: 'flashPhoto',
+          field: 'whole.flashPhoto',
           label: '闪照',
           helpMessage: '目前QQ群聊闪照功能已被移除',
           bottomHelpMessage: '开启后将转发群聊和私聊的闪照',
           component: 'Switch'
         },
         {
-          field: 'botBeenBanned',
+          field: 'whole.botBeenBanned',
           label: 'Bot被禁言',
           bottomHelpMessage: 'Bot在群聊被禁言后通知主人',
           component: 'Switch'
@@ -141,32 +141,32 @@ export function supportGuoba () {
           label: '其他设置'
         },
         {
-          field: 'Strangers_love',
+          field: 'whole.Strangers_love',
           label: '陌生人点赞',
           bottomHelpMessage: '开启后赞我功能将可以陌生人点赞，不活跃的号有可能被风控',
           component: 'Switch'
         },
         {
-          field: 'state',
+          field: 'whole.state',
           label: '默认状态',
           bottomHelpMessage: '是否将椰奶状态作为默认状态',
           component: 'Switch'
         },
         {
-          field: 'statusTask',
+          field: 'whole.statusTask',
           label: '状态任务',
           helpMessage: '关闭后不再读取网速和硬盘速率',
           bottomHelpMessage: '如果出现许多cmd进程占用或控制台输出乱码可尝试关闭此选项',
           component: 'Switch'
         },
         {
-          field: 'notificationsAll',
+          field: 'whole.notificationsAll',
           label: '通知全部管理',
           bottomHelpMessage: '开启后通知将会发送给所有主人',
           component: 'Switch'
         },
         {
-          field: 'deltime',
+          field: 'whole.deltime',
           label: '删除缓存时间',
           helpMessage: '删除撤回消息保存的时间',
           bottomHelpMessage: '不建议设置太久',
@@ -177,30 +177,117 @@ export function supportGuoba () {
         },
         {
           component: 'Divider',
-          label: 'pixiv设置'
+          label: '权限设置'
         },
         {
-          field: 'sese',
+          field: 'whole.sese',
           label: 'sese',
           bottomHelpMessage: '开放一些功能',
           component: 'Switch'
         },
         {
-          field: 'sesepro',
+          field: 'whole.sesepro',
           label: 'sesepro',
           bottomHelpMessage: '开放全部功能',
           component: 'Switch'
+        },
+        {
+          component: 'Divider',
+          label: 'pixiv设置'
+        },
+        {
+          field: 'pixiv.pixivDirectConnection',
+          label: 'pixiv图片直连',
+          bottomHelpMessage: '直接使用官方图片链接发送，请确保你的网络环境可以访问pixiv',
+          component: 'Switch'
+        },
+        {
+          field: 'pixiv.allowPM',
+          label: 'pixiv私聊使用',
+          bottomHelpMessage: 'pixiv是否允许私聊使用',
+          component: 'Switch'
+        },
+        {
+          field: 'pixiv.refresh_token',
+          label: 'pixiv登录刷新令牌',
+          bottomHelpMessage: '登录后直接使用账号调用官方api，不登录也可以正常使用功能',
+          component: 'Input'
+        },
+        {
+          field: 'pixiv.pixivImageProxy',
+          label: 'pixiv图片反代',
+          bottomHelpMessage: 'pixiv图片反代服务，如可以直接访问pixiv请直接打开图片直连',
+          component: 'Input'
+        },
+        {
+          field: 'pixiv.limit',
+          label: 'pixiv次数限制',
+          bottomHelpMessage: '每名用户每日次数限制（0 则无限制）',
+          component: 'InputNumber'
+        },
+        {
+          field: 'pixiv.language',
+          label: 'pixiv返回语言',
+          bottomHelpMessage: '返回语言, 会影响标签的翻译',
+          component: 'Input'
+        },
+        {
+          component: 'Divider',
+          label: '哔咔设置'
+        },
+        {
+          field: 'bika.allowPM',
+          label: '哔咔私聊使用',
+          bottomHelpMessage: '哔咔是否允许私聊使用',
+          component: 'Switch'
+        },
+        {
+          field: 'bika.bikaDirectConnection',
+          label: '哔咔图片直连',
+          bottomHelpMessage: '直接使用官方图片链接发送，请确保你的网络环境可以访问哔咔',
+          component: 'Switch'
+        },
+        {
+          field: 'bika.limit',
+          label: '哔咔次数限制',
+          bottomHelpMessage: '每名用户每日次数限制（0 则无限制）',
+          component: 'InputNumber'
+        },
+        {
+          field: 'bika.bikaImageProxy',
+          label: '哔咔图片反代',
+          bottomHelpMessage: '哔咔图片反代服务，如可以直接访问bika请直接打开图片直连',
+          component: 'Input'
+        },
+        {
+          field: 'bika.imageQuality',
+          label: '哔咔图片质量',
+          bottomHelpMessage: '哔咔返回的图片质量',
+          component: 'Select',
+          componentProps: {
+            options: [
+              { label: '低', value: 'low' },
+              { label: '中', value: 'medium' },
+              { label: '高', value: 'high' },
+              { label: '原图', value: 'original' }
+            ],
+            placeholder: '请选择图片质量'
+          }
         }
 
       ],
       // 获取配置数据方法（用于前端填充显示数据）
       getConfigData () {
-        return Config.Notice
+        return {
+          whole: Config.Notice,
+          pixiv: Config.pixiv,
+          bika: Config.bika
+        }
       },
 
       // 设置配置的方法（前端点确定后调用的方法）
       setConfigData (data, { Result }) {
-        for (let key in data) Config.modify('whole', key, data[key])
+        for (let key in data) Config.modify(...key.split('.'), data[key])
 
         return Result.ok({}, '保存成功辣ε(*´･ω･)з')
       }

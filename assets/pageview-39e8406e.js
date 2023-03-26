efccc94466f891fcb4bf5598ef79075832efeee6
yeyang52 <107110851+yeyang52@users.noreply.github.com>
2023-03-26 15:25:45 +0000

    Deploying to gh-pages from @ yeyang52/yenai-plugin@cef7aafb5e290864e6f8b4302b384dfe328c9426 🚀

--- assets/pageview-39e8406e.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./app-9ba0d14d.js";import"./framework-cc0a3056.js";const a=()=>e({serverURL:"https://waline.yenai.ren"});export{a as updatePageview};
---
+++ assets/pageview-89b2376e.js
@@ -0,0 +1,1 @@
+import{i as e}from"./app-62ff49a4.js";import"./framework-cc0a3056.js";const a=()=>e({serverURL:"https://waline.yenai.ren"});export{a as updatePageview};

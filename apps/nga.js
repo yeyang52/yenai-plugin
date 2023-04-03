e756fbb2428003ca2bfb5b7dc59ec7b6147e11da
yeyang <746659424@qq.com>
2023-04-03 15:06:40 +0800

    ✨ 修改参考面板资源地址

--- apps/nga.js
+++ apps/nga.js
@@ -74,7 +74,7 @@ export class NGA extends plugin {
     } else {
       // 参考面板
       imgList.push({
-        url: `http://www.liaobiao.top/Referenc/${role.name}.png`,
+        url: `https://gitlab.com/yeyang52/referenc-profile/-/raw/master/image/${role.name}.png`,
         imgPath: `${this.referencePanelPath}/${role.name}.png`
       })
     }

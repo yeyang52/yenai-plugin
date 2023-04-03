f21334daf4c983d07f00c132baeb6d2df710815d
时雨◎星空 <time.rain.star.sky@gmail.com>
2023-04-03 07:32:58 +0000

    ✨ !14 状态 非 Windows 默认 pro * 状态 非 Windows 默认 pro

--- model/State.js
+++ model/State.js
@@ -259,7 +259,7 @@ export default new class OSUtils {
 
   /** 获取FastFetch */
   async getFastFetch (e) {
-    if (!/pro/.test(e.msg)) return ''
+    if (process.platform == 'win32' && !/pro/.test(e.msg)) return ''
     let ret = await common.execSync('bash plugins/yenai-plugin/resources/state/state.sh')
     if (ret.error) {
       e.reply(`❎ 请检查是否使用git bash启动Yunzai-bot\n错误信息：${ret.stderr}`)

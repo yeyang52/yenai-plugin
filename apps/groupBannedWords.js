dfc43a80d7af8e621e27b215647ad7459ad7361e
yeyang <746659424@qq.com>
2023-04-07 15:49:56 +0800

    ⚗️ add

--- apps/groupBannedWords.js
+++ apps/groupBannedWords.js
@@ -3,20 +3,20 @@ import { common, GroupBannedWords } from '../model/index.js'
 export class NewGroupBannedWords extends plugin {
   constructor () {
     super({
-      name: '椰奶群屏蔽词',
+      name: '椰奶群违禁词',
       event: 'message.group',
       priority: 500,
       rule: [
         {
-          reg: '^#?新增屏蔽词.*$',
+          reg: '^#?新增(模糊|精确)?(踢|禁|撤|踢撤|禁撤)?违禁词.*$',
           fnc: 'add'
         },
         {
-          reg: '^#?删除屏蔽词.*$',
+          reg: '^#?删除违禁词.*$',
           fnc: 'del'
         },
         {
-          reg: '^#?查看屏蔽词.*$',
+          reg: '^#?查看违禁词.*$',
           fnc: 'query'
         }
       ]
@@ -26,13 +26,23 @@ export class NewGroupBannedWords extends plugin {
 
   async add (e) {
     if (!common.Authentication(e, 'admin', 'admin')) return false
-    let words = e.msg.replace(/#?新增屏蔽词/, '')?.split(',')
-    if (!words) return e.reply('需要添加的屏蔽词为空')
-    GroupBannedWords.addBannedWords(e.group_id, words)
+    let word = e.toString().match(/^#?新增(模糊|精确)?(踢|禁|撤|踢撤|禁撤)?违禁词(.*)$/)
+    if (!word[3]) return e.reply('需要添加的屏蔽词为空')
+    let res = GroupBannedWords.addBannedWords(e.group_id, word[3], word[1], word[2])
+    e.reply(
+      [
+        '✅ 成功添加违禁词\n',
+        `违禁词：${res.words}\n`,
+        `匹配模式：${res.matchType}\n`,
+        `处理方式：${res.penaltyType}`
+      ]
+    )
   }
 
   async del (e) {
-
+    if (!common.Authentication(e, 'admin', 'admin')) return false
+    let word = e.toString().replace(/#?删除违禁词/)
+    if (!word) return e.reply('需要删除的屏蔽词为空')
   }
 
   async query (e) {

64b60094a9f1b65cd70e0db435b1ae82b448f75a
yeyang <746659424@qq.com>
2023-04-25 21:33:01 +0800

    🚑 修复报错

--- model/index.js
+++ model/index.js
@@ -12,7 +12,7 @@ import Ascii2D from './PicSearch/ascii2d.js'
 import SauceNAO from './PicSearch/saucenao.js'
 import WhatAnime from './PicSearch/whatanime.js'
 import GroupBannedWords from './GroupBannedWords.js'
-import memes from './memes'
+import memes from './memes.js'
 // 导出模块
 export const PicSearch = {
   Ascii2D,

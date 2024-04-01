23d9565ad18686d35a54c5cf45269694a5d0d4b4
yeyang <746659424@qq.com>
2024-04-01 19:36:32 +0800

    🎨 优化时雨

--- model/State/BotState.js
+++ model/State/BotState.js
@@ -1,12 +1,11 @@
 import { formatDuration } from '../../tools/index.js'
-import { Version, Plugin_Name } from '../../components/index.js'
+import { Plugin_Name } from '../../components/index.js'
 import { status } from '../../constants/other.js'
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 
 export default async function getBotState (botList) {
   const defaultAvatar = `../../../../../plugins/${Plugin_Name}/resources/state/img/default_avatar.jpg`
-  const BotName = Version.name
 
   const dataPromises = botList.map(async (i) => {
     const bot = Bot[i]
@@ -24,15 +23,14 @@ export default async function getBotState (botList) {
     const friendQuantity = bot.fl?.size || 0
     const groupQuantity = bot.gl?.size || 0
     const groupMemberQuantity = Array.from(bot.gml?.values() || []).reduce((acc, curr) => acc + curr.size, 0)
-    const runTime = formatDuration(Date.now() / 1000 - bot.stat?.start_time, 'dd天hh小时mm分', false)
+    const botRunTime = formatDuration(Date.now() / 1000 - bot.stat?.start_time, 'dd天hh小时mm分ss秒', false)
     const botVersion = bot.version ? `${bot.version.name}(${bot.version.id})${bot.apk ? ` ${bot.version.version}` : ''}` : `ICQQ(QQ) v${require('icqq/package.json').version}`
 
     return {
       avatar,
       defaultAvatar,
       nickname,
-      BotName,
-      runTime,
+      botRunTime,
       onlineStatus,
       platform,
       botVersion,

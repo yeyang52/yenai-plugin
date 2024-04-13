4a60446267d5454fea5c6b6f7650f7e8eecb3c94
yeyang <746659424@qq.com>
2024-04-14 01:29:26 +0800

    💄 细节优化

--- model/State/BotState.js
+++ model/State/BotState.js
@@ -1,6 +1,5 @@
 import { formatDuration } from "../../tools/index.js"
 import { Plugin_Name } from "../../components/index.js"
-import { status } from "../../constants/other.js"
 import { createRequire } from "module"
 const require = createRequire(import.meta.url)
 
@@ -13,8 +12,7 @@ export default async function getBotState(botList) {
 
     const avatar = bot.avatar || (Number(bot.uin) ? `https://q1.qlogo.cn/g?b=qq&s=0&nk=${bot.uin}` : defaultAvatar)
     const nickname = bot.nickname || "未知"
-    const onlineStatus = status[bot.status] || "在线"
-    const platform = bot.apk ? `${bot.apk.display} v${bot.apk.version}` : bot.version?.version || "未知"
+    const platform = bot.apk ? `${bot.apk.display} v${extractVersion(bot.apk.version)}` : bot.version?.version || "未知"
 
     const sent = await redis.get(`Yz:count:send:msg:bot:${bot.uin}:total`) || await redis.get("Yz:count:sendMsg:total")
     const recv = await redis.get(`Yz:count:receive:msg:bot:${bot.uin}:total`) || bot.stat?.recv_msg_cnt
@@ -22,9 +20,10 @@ export default async function getBotState(botList) {
 
     const friendQuantity = bot.fl?.size || 0
     const groupQuantity = bot.gl?.size || 0
+    const onlineStatus = bot.status ?? 11
     const groupMemberQuantity = Array.from(bot.gml?.values() || []).reduce((acc, curr) => acc + curr.size, 0)
-    const botRunTime = formatDuration(Date.now() / 1000 - bot.stat?.start_time, "dd天hh小时mm分ss秒", false)
-    const botVersion = bot.version ? `${bot.version.name}(${bot.version.id})${bot.apk ? ` ${bot.version.version}` : ""}` : `ICQQ(QQ) v${require("icqq/package.json").version}`
+    const botRunTime = formatDuration(Date.now() / 1000 - bot.stat?.start_time, "dd天hh:mm:ss", true)
+    const botVersion = bot.version ? `${bot.version.name}${bot.apk ? ` ${bot.version.version}` : ""}` : `ICQQ v${require("icqq/package.json").version}`
 
     return {
       avatar,
@@ -44,4 +43,10 @@ export default async function getBotState(botList) {
   })
 
   return await Promise.all(dataPromises)
+}
+function extractVersion(versionString) {
+  const regex = /\d+\.\d+\.\d+/
+  const match = versionString.match(regex)
+  const version = match ? match[0] : versionString
+  return version
 }

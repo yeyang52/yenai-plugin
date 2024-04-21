5b5bcc9ea990e1a94c57b9c8ad6a3ea6442c5103
yeyang <746659424@qq.com>
2024-04-21 17:59:12 +0800

    💄 细节优化

--- model/State/BotState.js
+++ model/State/BotState.js
@@ -1,7 +1,8 @@
-import { formatDuration } from "../../tools/index.js"
-import { Plugin_Path } from "../../components/index.js"
 import { createRequire } from "module"
-import { importColorThief, getImgPalette } from "./utils.js"
+import moment from "moment"
+import { Plugin_Path } from "../../components/index.js"
+import { formatDuration } from "../../tools/index.js"
+import { getImgPalette, importColorThief } from "./utils.js"
 const require = createRequire(import.meta.url)
 
 export default async function getBotState(botList) {
@@ -13,18 +14,14 @@ export default async function getBotState(botList) {
     const avatar = await getAvatarColor(avatarUrl)
 
     const nickname = bot.nickname || "未知"
-    const platform = bot.apk ? `${bot.apk.display} v${extractVersion(bot.apk.version)}` : bot.version?.version || "未知"
-
-    const sent = await redis.get(`Yz:count:send:msg:bot:${bot.uin}:total`) || await redis.get("Yz:count:sendMsg:total")
-    const recv = await redis.get(`Yz:count:receive:msg:bot:${bot.uin}:total`) || bot.stat?.recv_msg_cnt
-    const screenshot = await redis.get(`Yz:count:send:image:bot:${bot.uin}:total`) || await redis.get("Yz:count:screenshot:total")
-
-    const friendQuantity = bot.fl?.size || 0
-    const groupQuantity = bot.gl?.size || 0
+    const platform = bot.apk ? `${bot.apk.display} v${bot.apk.ver}` : bot.version?.version || "未知"
+    const messageCount = await getMessageCount(bot)
+    const countContacts = getCountContacts(bot)
     const onlineStatus = bot.status ?? 11
-    const groupMemberQuantity = Array.from(bot.gml?.values() || []).reduce((acc, curr) => acc + curr.size, 0)
     const botRunTime = formatDuration(Date.now() / 1000 - bot.stat?.start_time, "dd天hh:mm:ss", true)
-    const botVersion = bot.version ? `${bot.version.name}${bot.apk ? ` ${bot.version.version}` : ""}` : `ICQQ v${require("icqq/package.json").version}`
+    const botVersion = bot.version
+      ? `${bot.version.name}${bot.apk ? ` ${bot.version.version}` : ""}`
+      : `ICQQ v${require("icqq/package.json").version}`
 
     return {
       avatar,
@@ -33,23 +30,19 @@ export default async function getBotState(botList) {
       onlineStatus,
       platform,
       botVersion,
-      recv,
-      sent,
-      screenshot,
-      friendQuantity,
-      groupQuantity,
-      groupMemberQuantity
+      messageCount,
+      countContacts
     }
   })
 
   return Promise.all(dataPromises)
 }
-function extractVersion(versionString) {
-  const regex = /\d+\.\d+\.\d+/
-  const match = versionString.match(regex)
-  const version = match ? match[0] : versionString
-  return version
-}
+// function extractVersion(versionString) {
+//   const regex = /\d+\.\d+\.\d+/
+//   const match = versionString.match(regex)
+//   const version = match ? match[0] : versionString
+//   return version
+// }
 
 async function getAvatarColor(url) {
   const defaultAvatar = `${Plugin_Path}/resources/state/img/default_avatar.jpg`
@@ -66,5 +59,27 @@ async function getAvatarColor(url) {
       similarColor2: "#ace0f9",
       path: url
     }
+  }
+}
+async function getMessageCount(bot) {
+  const nowDate = moment().format("MMDD")
+  const sent = await redis.get(`Yz:count:send:msg:bot:${bot.uin}:total`) || bot.stat?.sent_msg_cnt
+  const recv = await redis.get(`Yz:count:receive:msg:bot:${bot.uin}:total`) || bot.stat?.recv_msg_cnt
+  const screenshot = await redis.get(`Yz:count:send:image:bot:${bot.uin}:total`) || await redis.get(`Yz:count:screenshot:day:${nowDate}`)
+  return {
+    sent: sent || 0,
+    recv: recv || 0,
+    screenshot: screenshot || 0
+  }
+}
+
+function getCountContacts(bot) {
+  const friend = bot.fl?.size || 0
+  const group = bot.gl?.size || 0
+  const groupMember = Array.from(bot.gml?.values() || []).reduce((acc, curr) => acc + curr.size, 0)
+  return {
+    friend,
+    group,
+    groupMember
   }
 }

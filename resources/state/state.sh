f07ddbcca337fde0008130b186d071db8107091c
等风来 <3139373986@qq.com>
2024-05-12 15:18:54 +0800

    🎄 优化
    Close #I9OAB5
    
    Signed-off-by: 等风来 <3139373986@qq.com>

--- resources/state/state.sh
+++ resources/state/state.sh
@@ -17,7 +17,8 @@ elif command -v fastfetch &>/dev/null; then
     fastfetch --stdout | sed -n 's|: |</p><p>|p' | print_info
   fi
 else
-  curl -sSL https://gitee.com/TimeRainStarSky/neofetch/raw/master/neofetch | bash --stdout | sed -n 's|: |</p><p>|p' | print_info
+  bash <(curl -sL https://gitee.com/TimeRainStarSky/neofetch/raw/master/neofetch) --stdout | sed -n 's|: |</p><p>|p' | print_info
+
 fi
 
 echo "</div>"
@@ -25,11 +26,10 @@ echo "</div>"
 if command -v getprop &>/dev/null; then
   echo -n "<div class='box' data-boxInfo='安卓'>"
   echo "设备代号：$(getprop ro.product.device)
-  设备型号：$(getprop ro.product.marketname) ($(getprop ro.product.name))
-  认证型号：$(getprop ro.product.model)
-  安卓版本：$(getprop ro.build.version.release) (SDK $(getprop ro.build.version.sdk))
-  系统版本：$(getprop ro.build.version.incremental) ($(getprop ro.build.display.id))
-  编译时间：$(date -d "@$(getprop ro.build.date.utc)" "+%F %T")
-  基带版本：$(getprop gsm.version.baseband|cut -d ',' -f1)" | sed -n 's|：|</p><p>|p' | print_info
-  echo "</div>"
+设备型号：$(getprop ro.product.marketname) ($(getprop ro.product.name))
+认证型号：$(getprop ro.product.model)
+安卓版本：$(getprop ro.build.version.release) (SDK $(getprop ro.build.version.sdk))
+系统版本：$(getprop ro.build.version.incremental) ($(getprop ro.build.display.id))
+编译时间：$(date -d "@$(getprop ro.build.date.utc)" "+%F %T")
+基带版本：$(getprop gsm.version.baseband|cut -d ',' -f1)" 
 fi

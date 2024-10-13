22c80dcb4f770742d7b5df3ceb6b3c2d77228e5e
yeyang <746659424@qq.com>
2024-10-13 17:59:38 +0800

    ⚰️ 删除部分代码

--- apps/events/notice_friend.js
+++ apps/events/notice_friend.js
@@ -29,53 +29,6 @@ Bot.on?.("notice.friend", async(e) => {
       ]
       break
     }
-    /** 撤回 */
-    case "recall": {
-      if (!Config.getAlone(e.self_id).PrivateRecall) return false
-
-      if (e.user_id == (e.bot ?? Bot).uin) return false
-      // 主人撤回
-      if (Config.masterQQ.includes(e.user_id)) return false
-      logger.info("[Yenai-Plugin]好友撤回")
-      // 读取
-      let res = JSON.parse(
-        await redis.get(`notice:messagePrivate:${e.message_id}`)
-      )
-      // 无数据 return
-      if (!res) return false
-      const msgType = {
-        flash: {
-          msg: () => false,
-          type: [ "[闪照]\n", "撤回闪照：", segment.image(res[0].url) ]
-        },
-        record: {
-          msg: () => segment.record(res[0].url),
-          type: "[语音]"
-        },
-        video: {
-          msg: () => segment.video(res[0].file),
-          type: "[视频]"
-        },
-        xml: {
-          msg: () => res,
-          type: "[合并消息]"
-        }
-      }
-      if (msgType[res[0].type]) {
-        forwardMsg = msgType[res[0].type].msg()
-        res = msgType[res[0].type].type
-      }
-      // 消息
-      msg = [
-        segment.image(`https://q1.qlogo.cn/g?b=qq&s=100&nk=${e.user_id}`),
-        `[消息(${e.self_id}) - 好友撤回消息]\n`,
-          `好友账号：${e.user_id}\n`,
-          `撤回时间：${formatDate(e.time)}\n`,
-          "撤回消息：",
-          ...res
-      ]
-      break
-    }
     case "poke": {
       if (!Config.getAlone(e.self_id).privateMessage) return false
       logger.info("[Yenai-Plugin]好友戳一戳")
@@ -93,26 +46,3 @@ Bot.on?.("notice.friend", async(e) => {
   if (forwardMsg) await common.sendMasterMsg(forwardMsg, (e.bot ?? Bot).uin)
 }
 )
-
-/**
- * 时间转换
- * @param time
- */
-function formatDate(time) {
-  let now = new Date(parseFloat(time) * 1000)
-  // 月
-  let month = now.getMonth() + 1
-  // 日
-  let date = now.getDate()
-  // 补0
-  if (month >= 1 && month <= 9) month = "0" + month
-  if (date >= 0 && date <= 9) date = "0" + date
-  // 时
-  let hour = now.getHours()
-  // 分
-  let minute = now.getMinutes()
-  // 补0
-  if (hour >= 1 && hour <= 9) hour = "0" + hour
-  if (minute >= 0 && minute <= 9) minute = "0" + minute
-  return `${month}-${date} ${hour}:${minute} `
-}

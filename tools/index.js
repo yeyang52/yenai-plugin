6e70dae4eb13c06b0e994e47bb5b0eeb13064682
yeyang <746659424@qq.com>
2024-11-19 18:10:44 +0800

    ✨ feat(model): 重构 FastFetch 功能
    - 重新实现了 FastFetch 信息获取方法，增加了直接使用 fastfetch 命令的途径
    - 优化了错误处理和日志输出，提高了代码的健壮性和可维护性
    - 移除了 tools/index.js 中的 execSync 函数，减少了冗余代码
    - 更新了 package.json，移除了未使用的 colorthief 依赖

--- tools/index.js
+++ tools/index.js
@@ -3,7 +3,6 @@ import formatDuration from "./formatDuration.js"
 import sagiri from "./sagiri.js"
 import translateChinaNum from "./translateChinaNum.js"
 import uploadRecord from "./uploadRecord.js"
-import child_process from "child_process"
 
 /**
  * 延时函数
@@ -13,17 +12,4 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-/**
- * Promise执行exec
- * @param {string} cmd
- * @returns {*}
- */
-async function execSync(cmd) {
-  return new Promise((resolve, reject) => {
-    child_process.exec(cmd, (error, stdout, stderr) => {
-      resolve({ error, stdout, stderr })
-    })
-  })
-}
-
-export { cronValidate, formatDuration, sagiri, translateChinaNum, uploadRecord, sleep, execSync }
+export { cronValidate, formatDuration, sagiri, translateChinaNum, uploadRecord, sleep }

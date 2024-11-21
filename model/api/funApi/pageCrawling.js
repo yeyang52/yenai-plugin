e154d592e22b4151de81c31e577712fb0f10efb1
yeyang <746659424@qq.com>
2024-11-21 17:33:43 +0800

    ✨ refactor(model): 重构 PicSearch 和 funApi 模块
    - 移除对 cheerio 的动态导入，改为静态导入
    - 优化了 ascii2d、bgg、pageCrawling 等模块的代码结构
    - 删除了不必要的 _importDependency 函数

--- model/api/funApi/pageCrawling.js
+++ model/api/funApi/pageCrawling.js
@@ -1,7 +1,7 @@
 import { pandadiuType, xiurenTypeId } from "../../../constants/fun.js"
 import _ from "lodash"
-import { _importDependency } from "./utils.js"
 import request from "../../../lib/request/request.js"
+import { load } from "cheerio"
 
 /**
  *
@@ -9,7 +9,6 @@ import request from "../../../lib/request/request.js"
  * @param keywords
  */
 export async function pandadiu(type = "cos", keywords = "") {
-  let cheerio = await _importDependency()
   let domain = "https://www.pandadiu.com"
   const { id, page } = pandadiuType[type]
   let homeUrl = `${domain}/list-${id}-${_.random(1, page)}.html`
@@ -18,11 +17,11 @@ export async function pandadiu(type = "cos", keywords = "") {
   }
   logger.debug("[Yenai-Plugin][acg]作品索引页：" + homeUrl)
   const home = await request.get(homeUrl).then(res => res.text())
-  const href = _.sample(_.map(cheerio.load(home)("div.cos-list.clearfix > ul > a, div.cover.mod_imgLight > a"), item => item.attribs.href))
+  const href = _.sample(_.map(load(home)("div.cos-list.clearfix > ul > a, div.cover.mod_imgLight > a"), item => item.attribs.href))
   if (!href) throw new ReplyError("未找到结果")
   logger.debug("[Yenai-Plugin][acg]图片详情页：" + domain + href)
   const details = await request.get(domain + href).then(res => res.text())
-  const $ = cheerio.load(details)
+  const $ = load(details)
   const imgs = _.map($("div.con > p > img"), item =>
     segment.image(item.attribs.src.includes("http")
       ? item.attribs.src
@@ -40,7 +39,6 @@ export async function pandadiu(type = "cos", keywords = "") {
  * @param isSearch
  */
 export async function mengdui(keywords, isSearch) {
-  let cheerio = await _importDependency()
   const domain = "https://b6u8.com"
   let href = ""
   if (isSearch) {
@@ -48,7 +46,7 @@ export async function mengdui(keywords, isSearch) {
     const randomPage = _.random(1, mengduipage[keywords] || 1)
     const url = `${domain}/search.php?mdact=community&q=${keywords}&page=${randomPage}`
     const home = await request.get(url).then(res => res.text())
-    const $ = cheerio.load(home)
+    const $ = load(home)
     href = _.sample(_.map($("div.md-wide > ul > li > a"), item => item.attribs.href))
     if (!href) throw new ReplyError($("div.no-tips > p:nth-of-type(1)").text().trim())
     const maxPage = $("div.pagebar.md-flex-wc.mb20 > a:not(:last-child)").length
@@ -66,7 +64,7 @@ export async function mengdui(keywords, isSearch) {
     href = `${domain}/post/${random}.html`
   }
   const details = await request.get(href).then(res => res.text())
-  const $ = cheerio.load(details)
+  const $ = load(details)
   const imgs = _.map($("div.md-text.mb20.f-16 > p > img"), item => segment.image(item.attribs.src))
   const title = $("h1").text().trim()
   const number = `序号：${href.match(/(\d+).html/)[1]}`
@@ -78,12 +76,11 @@ export async function mengdui(keywords, isSearch) {
  * @param type
  */
 export async function xiuren(type) {
-  let cheerio = await _importDependency()
   // 可扩展
   let handleType = xiurenTypeId[type]
   let homeUrl = `https://www.lisiku1.com/forum-${handleType.id}-${_.random(1, handleType.maxPage)}.html`
   let html = await request.get(homeUrl).then(res => res.text())
-  let $ = cheerio.load(html)
+  let $ = load(html)
   let href = _.sample(
     _.map(
       $("#moderate > div > div.kind_show > div > div:nth-child(1) > a"),
@@ -94,7 +91,7 @@ export async function xiuren(type) {
   let imgPage = await request.get(imgPageUrl).then((res) =>
     res.text()
   )
-  let $1 = cheerio.load(imgPage)
+  let $1 = load(imgPage)
   let imgList = _.map(
     $1(
       "td > img"
@@ -107,12 +104,11 @@ export async function xiuren(type) {
  *
  */
 export async function coser() {
-  let cheerio = await _importDependency()
   const domain = "https://a2cy.com"
   const homeUrl = `${domain}/acg/cos/index_${_.random(1, 30)}.html`
   logger.debug("[Yenai-Plugin][coser]作品索引页：" + homeUrl)
   const home = await request.get(homeUrl).then(res => res.text())
-  const $ = cheerio.load(home)
+  const $ = load(home)
   const href = _.sample(
     _.map(
       $("body > div > div.content.hidden > ul.cy2-coslist.clr > li > div.showImg > a"),
@@ -122,7 +118,7 @@ export async function coser() {
   if (!href) throw new ReplyError("未知错误")
   logger.debug("[Yenai-Plugin][coser]图片详情页：" + domain + href)
   const imgPage = await request.get(domain + href).then(res => res.text())
-  const $1 = cheerio.load(imgPage)
+  const $1 = load(imgPage)
   const imgList = _.map(
     $1(
       "body > div > div.content.pb20.clr > div.cy_cosCon > div.w.maxImg.tc > p > img"

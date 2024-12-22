01b138fe13305b737fc9d9311e361b32baf729ba
yeyang <746659424@qq.com>
2024-12-22 19:56:57 +0800

    ⚡️ refactor(config): 重构配置文件处理逻辑
    - 新增 setData 方法批量设置配置数据
    - 优化 set 和 deleteKey 方法，支持数字键名
    - 重构配置数据处理流程，提高灵活性和可维护性
    - 新增辅助函数 convertToNestedObject 和 preprocessData
    - 优化 alone 数据处理逻辑，使用数字键名

--- components/YamlReader.js
+++ components/YamlReader.js
@@ -5,6 +5,8 @@ import chokidar from "chokidar"
 // import Constant from '../server/constant/Constant.js'
 
 export default class YamlReader {
+  // 配置文件数字key
+  static CONFIG_INTEGER_KEY = "INTEGER__"
   /**
    * 读写yaml文件
    * @param yamlPath yaml文件绝对路径
@@ -52,14 +54,15 @@ export default class YamlReader {
 
   /* 修改某个key的值 */
   set(keyPath, value, comment = null) {
+    keyPath = this.mapParentKeys(keyPath.split("."))
     if (!comment || this.get(keyPath) !== undefined) {
-      this.document.setIn(keyPath.split("."), value)
+      this.document.setIn(keyPath, value)
     } else {
-      this.document.addIn(keyPath.split("."), value)
+      this.document.addIn(keyPath, value)
     }
 
     if (comment) {
-      let seq = this.document.getIn(keyPath.split("."), true)
+      let seq = this.document.getIn(keyPath, true)
       if (!seq.comment) seq.comment = comment
     }
     this.save()
@@ -77,13 +80,53 @@ export default class YamlReader {
     this.save()
   }
 
+  /**
+   * 设置 document 的数据并保存（递归式）
+   * @param data 要写入的数据
+   */
+  setData(data) {
+    this.setDataRecursion(data, [])
+    this.save()
+  }
+
+  /**
+   * 递归式设置数据，但不保存
+   * @param data
+   * @param parentKeys
+   */
+  setDataRecursion(data, parentKeys) {
+    if (Array.isArray(data)) {
+      this.document.setIn(this.mapParentKeys(parentKeys), data)
+    } else if (typeof data === "object" && data !== null) {
+      for (const [ key, value ] of Object.entries(data)) {
+        this.setDataRecursion(value, parentKeys.concat([ key ]))
+      }
+    } else {
+      parentKeys = this.mapParentKeys(parentKeys)
+      this.document.setIn(parentKeys, data)
+    }
+  }
+
+  // 将数字key转为number类型，防止出现引号
+  mapParentKeys(parentKeys) {
+    return parentKeys.map((k) => {
+      if (k.startsWith(YamlReader.CONFIG_INTEGER_KEY)) {
+        const numericValue = Number(k.replace(YamlReader.CONFIG_INTEGER_KEY, ""))
+        if (!isNaN(numericValue)) {
+          return numericValue
+        }
+      }
+      return k
+    })
+  }
+
   // 彻底删除某个key
-  // deleteKey(keyPath) {
-  //   let keys = keyPath.split(".")
-  //   keys = this.mapParentKeys(keys)
-  //   this.document.deleteIn(keys)
-  //   this.save()
-  // }
+  deleteKey(keyPath) {
+    let keys = keyPath.split(".")
+    keys = this.mapParentKeys(keys)
+    this.document.deleteIn(keys)
+    this.save()
+  }
 
   save() {
     const yaml = this.document.toString()
--- guoba/schemas/index.js
+++ guoba/schemas/index.js
@@ -1,4 +1,5 @@
-import { Config } from "../../components/index.js"
+import { Config, YamlReader, Plugin_Path } from "../../components/index.js"
+import _ from "lodash"
 import notice from "./notice.js"
 import thumbUp from "./thumbUp.js"
 import state from "./state.js"
@@ -21,7 +22,7 @@ export const schemas = [
 ]
 
 export function getConfigData() {
-  const { groupAlone, botAlone, botAndGroupAlone } = getNoticeAlone()
+  const { groupAlone, botAlone, botAndGroupAlone } = parseNoticeAlone()
   return {
     notice: {
       default: Config.getNotice(),
@@ -36,68 +37,107 @@ export function getConfigData() {
     state: Config.state,
     proxy: Config.proxy,
     thumbUp: Config.thumbUp,
-    groupAdmin: getGroupAdmin()
+    groupAdmin: parseGroupAdmin()
   }
 }
-const getNoticeAlone = () => {
+function parseNoticeAlone() {
   const cfg = Config.getConfig("notice")
   const data = {
     groupAlone: [],
     botAlone: [],
     botAndGroupAlone: []
   }
-  for (let i in cfg) {
-    if (i == "default") continue
-    const d = cfg[i]
-    if (/bot:(.*):(.*)/.test(i)) {
-      const reg = /bot:(.*):(.*)/.exec(i)
+  for (let key in cfg) {
+    if (key == "default") continue
+    const d = cfg[key]
+    if (/bot:(.*):(.*)/.test(key)) {
+      const reg = /bot:(.*):(.*)/.exec(key)
       d.bot_id = reg[1]
       d.group_id = reg[2]
       data.botAndGroupAlone.push(d)
-    } else if (/bot:(.*)/.test(i)) {
-      const reg = /bot:(.*)/.exec(i)
+    } else if (/bot:(.*)/.test(key)) {
+      const reg = /bot:(.*)/.exec(key)
       d.bot_id = reg[1]
       data.botAlone.push(d)
     } else {
-      d.group_id = i
+      d.group_id = key
       data.groupAlone.push(d)
     }
   }
   return data
 }
-const getGroupAdmin = () => {
+const parseGroupAdmin = () => {
   const cfg = { ...Config.groupAdmin }
-  const r = []
-  for (let i in cfg.groupVerify.SuccessMsgs) {
-    r.push({
-      groupId: i,
-      msg: cfg.groupVerify.SuccessMsgs[i]
-    })
-  }
-  cfg.groupVerify.SuccessMsgs = r
+  cfg.groupVerify.SuccessMsgs = Object.entries(cfg.groupVerify.SuccessMsgs)
+    .map(([ groupId, msg ]) => ({ groupId, msg }))
   return cfg
 }
+
 export function setConfigData(data, { Result }) {
-  data = handleData(data)
-  for (let key in data) {
-    Config.modify(...key.split(/\.(.+)/, 2), data[key])
+  data = preprocessData(data)
+  const dataMap = convertToNestedObject(data)
+  for (let key in dataMap) {
+    const path = `${Plugin_Path}/config/config/${key}.yaml`
+    const y = new YamlReader(path)
+    y.setData(dataMap[key])
+    if (key == "notice") {
+      removeExcessAlone(y, dataMap.notice)
+    }
+    if (key == "groupAdmin") {
+      removeExcessAlone(y, dataMap.groupAdmin, "groupVerify.SuccessMsgs")
+    }
   }
-  logger.debug(data)
   return Result.ok({}, "保存成功辣ε(*´･ω･)з")
 }
 
-const handleData = (data) => {
+function removeExcessAlone(yamlReader, newData, path = "") {
+  const get = data => path ? _.get(data, path) : data
+  const existingData = get(yamlReader.jsonData)
+  const existingKeys = Object.keys(existingData)
+  const newKeys = Object.keys(get(newData)).map(key => key.replace(YamlReader.CONFIG_INTEGER_KEY, ""))
+  existingKeys.filter(key => !newKeys.includes(key)).forEach(key => {
+    if (/^\d+$/.test(key)) {
+      key = YamlReader.CONFIG_INTEGER_KEY + key
+    }
+    console.log(path ? `${path}.` : "" + key)
+    yamlReader.deleteKey((path ? `${path}.` : "") + key)
+  })
+}
+
+function convertToNestedObject(data) {
+  const result = {}
+
+  for (const key in data) {
+    if (Object.hasOwn(data, key)) {
+      const keys = key.split(".")
+      let obj = result
+
+      keys.forEach((k, index) => {
+        if (index === keys.length - 1) {
+          obj[k] = data[key]
+        } else {
+          obj[k] = obj[k] || {}
+          obj = obj[k]
+        }
+      })
+    }
+  }
+
+  return result
+}
+
+function preprocessData(data) {
   const SuccessMsgs = data["groupAdmin.groupVerify.SuccessMsgs"]
   if (SuccessMsgs?.length) {
     data["groupAdmin.groupVerify.SuccessMsgs"] = SuccessMsgs.reduce((r, i) => {
-      r[i.groupId] = i.msg
+      r[YamlReader.CONFIG_INTEGER_KEY + i.groupId] = i.msg
       return r
     }, {})
   }
   const noticeGroupAlone = data["notice.groupAlone"]
   if (noticeGroupAlone?.length) {
     noticeGroupAlone.forEach(e => {
-      const group_id = e.group_id
+      const group_id = YamlReader.CONFIG_INTEGER_KEY + e.group_id
       delete e.group_id
       data[`notice.${group_id}`] = e
     })

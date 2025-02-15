82f7e59a44c75e27d06627664160bf08983364f4
重装小兔 <103594311+OverTimeBunny@users.noreply.github.com>
2025-02-16 05:38:20 +0800

    频道消息处理
    频道消息处理，避免私聊环境下的错误推送

--- apps/events/message.js
+++ apps/events/message.js
@@ -1,121 +1,121 @@
-import { common } from "../../model/index.js"
-import { Config, Log_Prefix } from "#yenai.components"
+import { common } from "../../model/index.js";
+import { Config } from "../../components/index.js";
 
-Bot.on?.("message", async(e) => {
-  // 判断是否存在消息
-  if (!e?.message?.length) return false
-  // 判断是否为机器人消息
-  if (e.user_id == (e.bot ?? Bot).uin) return false
-  // 判断是否主人消息
-  if (Config.masterQQ.includes(e.user_id)) return false
-  // 消息通知
-  let msg = null
-  let specialMsg = null
-  let rawMsg = null
-  const cfg = Config.getNotice(e.self_id, e.group_id)
-  // 特殊消息处理
-  const msgType = getMsgType(e.message)
-  if (msgType) {
-    specialMsg = msgType.msg
-    rawMsg = [ msgType.type ]
-  } else {
-    rawMsg = e.message
-  }
+Bot.on?.("message", async (e) => {
+    // 判断是否存在消息
+    if (!e?.message?.length) return false;
+    // 判断是否为机器人消息
+    if (e.user_id == (e.bot ?? Bot).uin) return false;
+    // 判断是否为频道消息（排除频道消息）
+    if (e.message_type === "guild") return false;
+    // 判断是否为群聊或私聊消息
+    if (e.message_type !== "group" && e.message_type !== "private") return false;
+    // 判断是否为主人消息
+    if (Config.masterQQ.includes(e.user_id)) return false;
 
-  switch (e.message_type) {
-    case "group": {
-      if (!cfg.groupMessage) return false
-      logger.info(`${Log_Prefix} 群聊消息`)
-      msg = [
-        segment.image(`https://p.qlogo.cn/gh/${e.group_id}/${e.group_id}/100`),
-      `[消息(${e.self_id}) - 群聊消息]\n`,
-      `来源群号：${e.group_id}\n`,
-      `来源群名：${e.group_name}\n`,
-      `发送人账号：${e.user_id}\n`,
-      `发送人昵称：${e.sender.nickname}\n`,
-      "消息内容：",
-      ...rawMsg
-      ]
-      break
-    }
-    case "private": {
-      if (e.sub_type === "friend") {
-        if (!cfg.privateMessage) return false
+    // 消息通知
+    let msg = null;
+    let forwardMsg = null;
 
-        logger.info(`${Log_Prefix} 好友消息`)
-        msg = [
-          segment.image(`https://q1.qlogo.cn/g?b=qq&s=100&nk=${e.user_id}`),
-          `[消息(${e.self_id}) - 好友消息]\n`,
-          `好友账号：${e.user_id}\n`,
-          `好友昵称：${e.sender.nickname}\n`,
-          "消息内容：",
-          ...rawMsg
-        ]
-        // 添加提示消息
-        const key = `yenai:notice:privateMessage:${e.user_id}`
-        if (!(await redis.get(key))) {
-          await redis.set(key, "1", { EX: 600 })
-          msg.push(
-            "\n-------------\n",
-            "引用该消息：回复 <内容>\n",
-        `或发送:回复 ${e.user_id} <内容>`
-          )
+    switch (e.message_type) {
+        case "group": {
+            if (!Config.getAlone(e.self_id, e.group_id).groupMessage) return false;
+            const arr = getMsgType(e.message);
+            if (arr) {
+                forwardMsg = arr.msg;
+                e.message = arr.请键入;
+            }
+            logger.info("[Yenai-Plugin]群聊消息");
+            msg = [
+                segment.image(`https://p.qlogo.cn/gh/${e.group_id}/${e.group_id}/100`),
+                `[消息(${e.self_id}) - 群聊消息]\n`,
+                `来源群号：${e.group_id}\n`,
+                `来源群名：${e.group_name}\n`,
+                `发送人账号：${e.user_id}\n`,
+                `发送人昵称：${e.sender.nickname}\n`,
+                "消息内容：",
+                ...e.message
+            ];
+            break;
         }
-      } else if (e.sub_type === "group") {
-        if (!cfg.grouptemporaryMessage) return false
-        logger.info(`${Log_Prefix} 群临时消息`)
-        // 发送的消息
-        msg = [
-          segment.image(`https://q1.qlogo.cn/g?b=qq&s=100&nk=${e.user_id}`),
-          `[消息(${e.self_id}) - 群临时消息]\n`,
-          `来源群号：${e.sender.group_id}\n`,
-          `发送人账号：${e.user_id}\n`,
-          "消息内容：",
-          ...rawMsg
-        ]
-        // 添加提示消息
-        const key = `yenai:notice:tempprivateMessage:${e.user_id}`
-        if (!(await redis.get(key))) {
-          await redis.set(key, "1", { EX: 600 })
-          msg.push(
-            "\n-------------\n",
-            "可回复 \"加为好友\" 添加好友\n或 \"回复 <消息>\""
-          )
+        case "private": {
+            if (e.sub_type === "friend") {
+                if (!Config.getAlone(e.self_id).privateMessage) return false;
+                const arr = getMsgType(e.message);
+                if (arr) {
+                    forwardMsg = arr.msg;
+                    e.message = arr.请键入;
+                }
+                logger.info("[Yenai-Plugin]好友消息");
+                msg = [
+                    segment.image(`https://q1.qlogo.cn/g?b=qq&s=100&nk=${e.user_id}`),
+                    `[消息(${e.self_id}) - 好友消息]\n`,
+                    `好友账号：${e.user_id}\n`,
+                    `好友昵称：${e.sender.nickname}\n`,
+                    "消息内容：",
+                    ...e.message
+                ];
+                const key = `yenai:notice:privateMessage:${e.user_id}`;
+                if (!(await redis.get(key))) {
+                    await redis.set(key, "1", { EX: 600 });
+                    msg.push(
+                        "\n-------------\n",
+                        "引用该消息：回复 <内容>\n",
+                        `或发送:回复 ${e.user_id} <内容>`
+                    );
+                }
+            } else if (e.sub_type === "group") {
+                if (!Config.getAlone(e.self_id, e.group_id).grouptemporaryMessage) return false;
+                const arr = getMsgType(e.message);
+                if (arr) {
+                    forwardMsg = arr.msg;
+                    e.message = arr.请键入;
+                }
+                logger.info("[Yenai-Plugin]群临时消息");
+                msg = [
+                    segment.image(`https://q1.qlogo.cn/g?b=qq&s=100&nk=${e.user_id}`),
+                    `[消息(${e.self_id}) - 群临时消息]\n`,
+                    `来源群号：${e.sender.group_id}\n`,
+                    `发送人账号：${e.user_id}\n`,
+                    "消息内容：",
+                    ...e.message
+                ];
+                const key = `yenai:notice:tempprivateMessage:${e.user_id}`;
+                if (!(await redis.get(key))) {
+                    await redis.set(key, "1", { EX: 600 });
+                    msg.push(
+                        "\n-------------\n",
+                        "可回复 \"加为好友\" 添加好友\n或 \"回复 <消息>\""
+                    );
+                }
+            }
+            break;
         }
-      }
-      break
     }
-    default:
-      return false
-  }
-  if (!msg) return
-  // 发送消息
-  await common.sendMasterMsg(msg, (e.bot ?? Bot).uin)
-  if (specialMsg) await common.sendMasterMsg(specialMsg, (e.bot ?? Bot).uin)
-})
+
+    // 发送消息
+    if (msg) await common.sendMasterMsg(msg, (e.bot ?? Bot).uin);
+    if (forwardMsg) await common.sendMasterMsg(forwardMsg, (e.bot ?? Bot).uin);
+});
 
 /**
  * 特殊消息处理
  * @param msg
  */
 function getMsgType(msg) {
-  const msgType = {
-    record: {
-      msg: segment.record(msg[0].url),
-      type: "[语音]"
-    },
-    video: {
-      msg: segment.video(msg[0].file),
-      type: "[视频]"
-    },
-    xml: {
-      msg,
-      type: "[合并消息]"
-    },
-    json: {
-      msg,
-      type: "[JSON]"
-    }
-  }
-  return msgType[msg[0].type]
+    const msgType = {
+        record: {
+            msg: segment.record(msg[0].url),
+            请键入: "[语音]"
+        },
+        video: {
+            msg: segment.video(msg[0].file),
+            请键入: "[视频]"
+        },
+        xml: {
+            msg,
+            请键入: "[合并消息]"
+        }
+    };
+    return msgType[msg[0].请键入];
 }
